aa8bbd78641f3b1195658250fcee94ea638f23a9
Robert076 <robertberes06@gmail.com>
2025-03-12 15:42:40 +0200

    refactor: new structure

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-export default function App() {
+const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world</Text>
     </View>
   );
-}
+};
+
+export default Home;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
 });
