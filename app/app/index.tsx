f3f61563355ac917b7fb62a9ad852f412ef88696
Robert076 <robertberes06@gmail.com>
2025-03-13 09:10:05 +0200

    feat: done custom text input

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -2,12 +2,14 @@ import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import React from "react";
 // @ts-ignore
 import backgroundImage from "@/assets/background.png";
+import CustomTextInput from "@/components/CustomTextInput";
 
 const App = () => {
   return (
     <View style={styles.container}>
       <ImageBackground source={backgroundImage} style={styles.bgImage}>
         <Text>Hello World!</Text>
+        <CustomTextInput text1="Weight" text2="kilograms" />
       </ImageBackground>
     </View>
   );
