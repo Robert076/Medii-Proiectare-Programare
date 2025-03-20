c1b04b824354d156c48348449854f92950ff67ce
Robert076 <robertberes06@gmail.com>
2025-03-20 08:16:22 +0200

    refactor

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,22 +1,5 @@
 import { Slot, Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerTitle: "Home",
-          headerStyle: { backgroundColor: "#B7A06E" },
-        }}
-      />
-      <Stack.Screen
-        name="add/index"
-        options={{
-          headerTitle: "Add gun",
-          headerStyle: { backgroundColor: "#B7A06E" },
-        }}
-      />
-    </Stack>
-  );
+  return <Slot />;
 }
--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,30 +1,14 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { router } from "expo-router";
 
 const index = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>index</Text>
-      <Pressable
-        onPress={() => {
-          router.push({
-            pathname: "/add",
-          });
-        }}
-      >
-        <Text>Go to add</Text>
-      </Pressable>
     </View>
   );
 };
 
 export default index;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+const styles = StyleSheet.create({});
