1cf4ebaff81349aced1dc9499cac403b827a1ef5
Robert076 <robertberes06@gmail.com>
2025-03-20 08:16:27 +0200

    Revert "refactor"
    This reverts commit c1b04b824354d156c48348449854f92950ff67ce.

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,5 +1,22 @@
 import { Slot, Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: "Home",
+          headerStyle: { backgroundColor: "#B7A06E" },
+        }}
+      />
+      <Stack.Screen
+        name="add/index"
+        options={{
+          headerTitle: "Add gun",
+          headerStyle: { backgroundColor: "#B7A06E" },
+        }}
+      />
+    </Stack>
+  );
 }
--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { router } from "expo-router";
 
 const index = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>index</Text>
+      <Pressable
+        onPress={() => {
+          router.push({
+            pathname: "/add",
+          });
+        }}
+      >
+        <Text>Go to add</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default index;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
